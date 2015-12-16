4c631ba4108733d084675c965ccb3e45b85a3bd0
JuliaBalashova <juliabalashova@Julias-MacBook-Pro-3.local>
2015-12-16 16:22:15 +0200

    upd2

---
+++ Assets/ScriptStop.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptStop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log ("3");
+		Debug.Log ("4");	
+	}
+}
