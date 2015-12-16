6eac3cd4ca35b961bc51dab0250145471d86767f
JuliaBalashova <juliabalashova@Julias-MacBook-Pro-3.local>
2015-12-16 16:32:04 +0200

    upd3

---
+++ Assets/ScriptUp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log ("333");
+		Debug.Log ("444");	
+	}
+}
